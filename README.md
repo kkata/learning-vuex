e42eae087842142c1215a3bc217a041b52d838d1
kkata <14849535+kkata@users.noreply.github.com>
2020-11-21 15:45:22 +0900

    learned mutations and actions 1

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # learning vuex
 
 Learned vuex at [Intro to Vuex \- Mastering Vuex \| Vue Mastery](https://www.vuemastery.com/courses/mastering-vuex/intro-to-vuex).
-
-## docs
-
-[3. State & Getters](./docs/03-state-and-getters.md)
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService'
 
 Vue.use(Vuex)
 
@@ -15,28 +16,21 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: [
-      { id: 1, title: '...', organizer: '...' },
-      { id: 2, title: '...', organizer: '...' },
-      { id: 3, title: '...', organizer: '...' },
-      { id: 4, title: '...', organizer: '...' }
-    ]
-    // todos: [
-    //   { id: 1, text: '...', done: true },
-    //   { id: 2, text: '...', done: false },
-    //   { id: 3, text: '...', done: true },
-    //   { id: 4, text: '...', done: false }
-    // ]
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   getters: {
-    // catLength: state => {
-    //   return state.categories.length
-    // },
-    // activeTodosCount: state => {
-    //   return state.todos.filter(todo => !todo.done).length
-    // }
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
     }
